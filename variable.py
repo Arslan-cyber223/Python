49ff072cfcdc38ea30f99d691f45cb6ecd70ff71
Arslan Hassan <hassanarslan212@gmail.com>
2025-03-12 12:23:00 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,25 @@
+a=12
+print(a)
+b="Arslan"
+print(b)
+c=76
+print(a+c)
+d=True
+print(d)
+e=None
+print(e)
+print("The type of a is : ",type(a))
+print("The type of b is : ",type(b))
+print("The type of d is : ",type(d))
+print("The type of e is : ",type(e))\
+    
+j=complex(2,4)
+print(j)
+
+print("The type of j is : ",type(j))
+
+list1=[12,23,[-2,3],["Banana","Apple"]]
+print(list1)
+
+dict1={"Name" : "Sakshi","Age ":20 ,"Canvote":True}
+print(dict1)
